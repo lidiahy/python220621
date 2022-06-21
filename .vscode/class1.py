8f8e7490d1153eef534653e252b4061b7c7fda8b
lidiahy <heeyelee@gmail.com>
2022-06-21 14:37:12 +0900

    클래스 연습 4

--- .vscode/class1.py
+++ .vscode/class1.py
@@ -15,3 +15,11 @@ p2 = Person()
 p1.print()
 p2.name = "RafaelNadal"
 p2.print()
+
+# 런타임시에 변수 추가
+# 통상 정적 언어에서는 title 변수 걸면 syntax error 발생
+# 파이썬은 개발자가 필요해서 입력했겠거니~ 하고 말음
+Person.title = "new title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
