1dd5dd9c92c26a4ab69a5d111daecedd4d3594ea
lidiahy <heeyelee@gmail.com>
2022-06-22 10:21:25 +0900

    에러처리 연습

---
+++ try1.py
@@ -0,0 +1,25 @@
+# try1.py
+# 함수 정의
+def divide(a,b):
+    return a/b
+
+# 함수 호출
+# result = divide(5,0)
+# print("결과: {0}".format(result))
+
+# print("---전체 코드 실행 종료---")
+# 위에거는 에러남
+
+# 에러 처리
+try :
+    # 함수 호출
+    result = divide(5,0)
+    print("결과:{0}".format(result))
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다.")
+except TypeError:
+    print("숫자여야 합니다.")
+else:
+    print("결과:{0}".format(result))
+finally:
+    print("무조건 실행(한번 더 체크")
